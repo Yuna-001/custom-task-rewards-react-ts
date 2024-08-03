62897cf3f295a442d51822e8da4ff57b8b7a3f4f
Lee Yuna <leeyuna7689@gmail.com>
2024-08-04 00:17:40 +0900

    미디어 쿼리 중단점 변경

--- src/media.ts
+++ src/media.ts
@@ -3,9 +3,9 @@ import { css, type CSSObject, type Interpolation } from "styled-components";
 export type Breakpoints = "small" | "medium" | "large";
 
 export const breakpoints: Record<Breakpoints, string> = {
-  small: "@media (max-width: 639px)",
-  medium: "@media (max-width: 1047px)",
-  large: "@media (min-width: 1048px)",
+  small: "@media (max-width: 767px)",
+  medium: "@media (max-width: 1023px)",
+  large: "@media (min-width: 1024px)",
 };
 
 const media = Object.entries(breakpoints).reduce((acc, [key, value]) => {
