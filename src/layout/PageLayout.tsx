4a2e265bae45d956ea61f309a334ba8045b2211a
Lee Yuna <leeyuna7689@gmail.com>
2024-08-07 01:38:11 +0900

    PageLayout 스타일링

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -1,7 +1,26 @@
 import { ReactNode } from "react";
+import styled from "styled-components";
+
+import media from "../media";
+
+const Main = styled.main`
+  width: 80%;
+  margin-top: 3.5rem;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(14rem, 1fr));
+  gap: 3rem;
+  align-items: center;
+  ${media.small`
+    width : 80%;
+    grid-template-columns: repeat(auto-fit, minmax(15rem, auto));
+  `}
+`;
 
 const PageLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <main>{children}</main>;
+  return <Main>{children}</Main>;
 };
 
 export default PageLayout;
