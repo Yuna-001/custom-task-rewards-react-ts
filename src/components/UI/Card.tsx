f249641951426b2d2bc602c7757481cbb57fc69f
Lee Yuna <leeyuna7689@gmail.com>
2024-08-30 01:16:05 +0900

    카드 레이아웃 완성

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Card = styled.section`
+  width: 100%;
+  height: 15rem;
+  background-color: #e4e0d5;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  border-radius: 1rem;
+`;
+
+export default Card;
