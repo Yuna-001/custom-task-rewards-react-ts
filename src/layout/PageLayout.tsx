45b2f5c1612a48ba52d5c07c1111a6459dc9fff3
Lee Yuna <leeyuna7689@gmail.com>
2024-09-14 23:12:37 +0900

    PageLayout의 css 수정
    - Card 컴포넌트의 개수가 적은 경우, Card 컴포넌트의 가로 길이가 매우 길어진다는 문제를 해결하기 위해 PageLayout의 grid-template-columns을 반응형으로 수정

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -9,11 +9,17 @@ const PageLayout = styled.main`
   left: 50%;
   transform: translateX(-50%);
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(14rem, 1fr));
+  grid-template-columns: repeat(3, 1fr);
   gap: 3rem;
   align-items: center;
+  justify-content: left;
+
+  ${media.medium`
+    grid-template-columns: repeat(2, 1fr);
+  `}
+
   ${media.small`
-    grid-template-columns: repeat(auto-fit, minmax(15rem, auto));
+    grid-template-columns: 1fr;
   `}
 `;
 
