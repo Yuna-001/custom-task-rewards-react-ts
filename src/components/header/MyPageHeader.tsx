10a42fea81ab4d7a7c289a01b9273ad1b1010dc6
Lee Yuna <leeyuna7689@gmail.com>
2024-11-24 03:16:47 +0900

    feat: MyPageLayout과 MyPageHeader 스타일링 완료

--- src/components/header/MyPageHeader.tsx
+++ src/components/header/MyPageHeader.tsx
@@ -1,23 +1,58 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 import usePath from "../../hooks/usePath";
+
+const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Nav = styled.nav`
+  padding: 1rem 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const LinkList = styled.ul`
+  padding: 0;
+  display: flex;
+  gap: 2rem;
+`;
+
+const LinkItem = styled(NavLink)`
+  color: #74726e;
+  transition-duration: 0.2s;
+
+  &.active {
+    color: black;
+    border-bottom: 1px solid black;
+  }
+  &:hover {
+    color: black;
+  }
+`;
 
 const MyPageHeader = () => {
   const { userId } = usePath();
 
   return (
-    <header>
-      <nav>
-        <ul>
+    <Header>
+      <Nav>
+        <LinkList>
           <li>
-            <NavLink to={`/${userId}/my-page`}>대시보드</NavLink>
+            <LinkItem to={`/${userId}/my-page`} end>
+              대시보드
+            </LinkItem>
           </li>
           <li>
-            <NavLink to={`/${userId}/my-page/setting`}>설정</NavLink>
+            <LinkItem to={`/${userId}/my-page/setting`}>설정</LinkItem>
           </li>
-        </ul>
-      </nav>
-    </header>
+        </LinkList>
+      </Nav>
+    </Header>
   );
 };
 
--- src/components/layout/MyPageLayout.tsx
+++ src/components/layout/MyPageLayout.tsx
@@ -4,6 +4,7 @@ import MyPageHeader from "../header/MyPageHeader";
 
 const Layout = styled.main`
   width: 80%;
+  padding-bottom: 4rem;
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
