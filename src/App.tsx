cbd323e3f895d4200e3a60ccbd5aad4f5035d33f
Lee Yuna <leeyuna7689@gmail.com>
2024-11-26 21:57:27 +0900

    chore: 불필요한 import문 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
+import { queryClient } from "./utils/http";
 import AuthPage from "./pages/AuthPage";
 import authAction from "./components/auth/authActions";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 import RootLayout from "./components/layout/RootLayout";
 import MainPage from "./pages/MainPage";
 import ItemForm from "./components/items/ItemForm";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import { queryClient } from "./utils/http";
 import MyPageLayout from "./components/layout/MyPageLayout";
 import SettingPage from "./pages/SettingPage";
 import DashboardPage from "./pages/DashboardPage";
