7c381251ce3d7373f885faf73a39ea58876609fc
Lee Yuna <leeyuna7689@gmail.com>
2024-11-27 00:53:09 +0900

    chore: chunk 분할 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,18 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    chunkSizeWarningLimit: 650,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            const module = id.split("/node_modules/")[1].split("/")[0];
+
+            return `vendor-${module}`;
+          }
+        },
+      },
+    },
+  },
 });
